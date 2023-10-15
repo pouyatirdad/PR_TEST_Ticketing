794b55bed53e3d0373ea13f844a68275312ca866
pouya tirdad <pouyatirdad70@gmail.com>
2023-10-15 22:19:19 +0330

    Add project files.

---
+++ Ticketing.Test/TicketBookingRequestHandlerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Test
+{
+    public class Ticket_Booking_Request_Handler_Test
+    {
+        public void Should_Return_Ticket_Booking_Response_With_Request_Values()
+        {
+            //arrange - collect data
+
+
+            //act - pass data to method
+
+
+            //assert - pass tests
+        }
+    }
+}
