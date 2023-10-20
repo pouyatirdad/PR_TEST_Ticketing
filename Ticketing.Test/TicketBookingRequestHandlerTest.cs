49f4b3cd24e918af5e3c3dfe4deb81af075d8f0e
pouya tirdad <pouyatirdad70@gmail.com>
2023-10-20 17:16:12 +0330

    write new test and add moq

--- Ticketing.Test/TicketBookingRequestHandlerTest.cs
+++ Ticketing.Test/TicketBookingRequestHandlerTest.cs
@@ -2,47 +2,54 @@
 using Shouldly;
 using Ticketing.Core.Model;
 using Ticketing.Core.Handler;
+using Ticketing.Core.DataService;
+using Moq;
+using Ticketing.Core.Domain;
 
 namespace Ticketing.Test
 {
     public class Ticket_Booking_Request_Handler_Test
     {
         private readonly TicketBookingRequestHandler _handler;
+        private readonly TicketBookingRequest _request;
+        private readonly Mock<ITicketBookingService> _ticketBookingServiceMock;
 
         public Ticket_Booking_Request_Handler_Test()
         {
-            _handler = new TicketBookingRequestHandler();
+            //arrange - collect data
+
+            _request = new TicketBookingRequest
+            {
+                Name = "Test Name",
+                Family = "Test Family",
+                Email = "Test Email",
+            };
+
+            _ticketBookingServiceMock = new Mock<ITicketBookingService>();
+
+            _handler = new TicketBookingRequestHandler(_ticketBookingServiceMock.Object);
         }
 
         [Fact]
         public void Should_Return_Ticket_Booking_Response_With_Request_Values()
         {
-            //arrange - collect data
-
-            var BookingRequest = new TicketBookingRequest
-            {
-                Name= "Test Name",
-                Family= "Test Family",
-                Email= "Test Email",
-            };
-
             //act - pass data to method
 
-            ServiceBookingResult Result =_handler.BookService(BookingRequest);
+            ServiceBookingResult Result =_handler.BookService(_request);
 
             //assert - pass tests
 
             Assert.NotNull(Result);
-            Assert.Equal(BookingRequest.Name, Result.Name);
-            Assert.Equal(BookingRequest.Family, Result.Family);
-            Assert.Equal(BookingRequest.Email, Result.Email);
+            Assert.Equal(_request.Name, Result.Name);
+            Assert.Equal(_request.Family, Result.Family);
+            Assert.Equal(_request.Email, Result.Email);
 
                //use shouldbe
 
             Result.ShouldNotBeNull();
-            Result.Name.ShouldBe(BookingRequest.Name);
-            Result.Family.ShouldBe(BookingRequest.Family);
-            Result.Email.ShouldBe(BookingRequest.Email);
+            Result.Name.ShouldBe(_request.Name);
+            Result.Family.ShouldBe(_request.Family);
+            Result.Email.ShouldBe(_request.Email);
         }
 
         [Fact]
@@ -50,6 +57,29 @@ namespace Ticketing.Test
         {
             var exception = Should.Throw<ArgumentNullException>(()=> _handler.BookService(null));
             exception.ParamName.ShouldBe("bookingRequest");
+
+        }
+
+        [Fact]
+        public void Should_Save_Ticket_Booking_Request()
+        {
+            TicketBooking savedBooking = null;
+            _ticketBookingServiceMock.Setup(x => x.Save(It.IsAny<TicketBooking>()))
+                .Callback<TicketBooking>(result =>
+                {
+                    savedBooking = result;
+                });
+
+            _handler.BookService(_request);
+
+            _ticketBookingServiceMock.Verify(x=>x.Save(It.IsAny<TicketBooking>()),Times.Once);
+
+            //assert
+
+            savedBooking.ShouldNotBeNull();
+            savedBooking.Name.ShouldBe(_request.Name);
+            savedBooking.Family.ShouldBe(_request.Family);
+            savedBooking.Email.ShouldBe(_request.Email);
 
         }
     }
