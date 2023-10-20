458304d8b3eeac9004c30dc5b76114ff1119151d
pouya tirdad <pouyatirdad70@gmail.com>
2023-10-20 16:53:52 +0330

    add interface and model

---
+++ Ticketing.Test.Core/Domain/TicketBooking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticketing.Core.Domain
+{
+    public class TicketBooking
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Email { get; set; }
+    }
+}
