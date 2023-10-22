e506b0b5e04a62f1cad3534f2cf333f9ee594e4b
pouya tirdad <pouyatirdad70@gmail.com>
2023-10-22 21:07:06 +0330

    add method in service for pass test

--- Ticketing.Test.Core/Handler/TicketBookingRequestHandler.cs
+++ Ticketing.Test.Core/Handler/TicketBookingRequestHandler.cs
@@ -20,7 +20,13 @@ namespace Ticketing.Core.Handler
                 throw new ArgumentNullException(nameof(bookingRequest));
             }
 
-            _ticketBookingService.Save(CreateTicketBookingObject<TicketBooking>(bookingRequest));
+            var availableTickets = _ticketBookingService.GetAvailableTickets(bookingRequest.Date);
+
+            if (availableTickets.Any())
+            {
+                _ticketBookingService.Save(CreateTicketBookingObject<TicketBooking>(bookingRequest));
+            }
+
 
             return CreateTicketBookingObject<ServiceBookingResult>(bookingRequest);
         }
