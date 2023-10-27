be82a777a2a48e961d90e9a1feb3dfef5ff224a9
pouya tirdad <pouyatirdad70@gmail.com>
2023-10-27 15:50:56 +0330

    fix something

--- Ticketing.Api/Program.cs
+++ Ticketing.Api/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Ticketing.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connString ="DataSource=:memory:";
+var conn =new SqliteConnection(connString);
+conn.Open();
+
+builder.Services.AddDbContext<MyDbContext>(o=>o.UseSqlite(conn));
 
 var app = builder.Build();
 
